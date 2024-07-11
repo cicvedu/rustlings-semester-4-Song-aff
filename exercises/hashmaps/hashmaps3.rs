e7a99c1db08f7fb6582e264bbd60b2595ec9ecb5
doki <281218023@qq.com>
2024-07-12 00:41:04 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,7 +39,32 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // let team_1 =  scores.entry(team_1_name).or_insert(Team {goals_scored:team_1_score,goals_conceded:team_2_score});
+     
+        // let team_2 =  scores.entry(team_2_name).or_insert(Team {goals_scored:team_2_score,goals_conceded:team_1_score});
+        
+        // // 更新或插入team_1的得分
+        // scores.entry(team_1_name).and_modify(|team| {
+        //     team.goals_scored += team_1_score;
+        //     team.goals_conceded += team_2_score;
+        // }).or_insert(Team {goals_scored: team_1_score, goals_conceded: team_2_score});
+
+        // // 更新或插入team_2的得分
+        // scores.entry(team_2_name).and_modify(|team| {
+        //     team.goals_scored += team_2_score;
+        //     team.goals_conceded += team_1_score;
+        // }).or_insert(Team {goals_scored: team_2_score, goals_conceded: team_1_score});
+
+        let t1 = scores.entry(team_1_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        t1.goals_scored += team_1_score;
+        t1.goals_conceded += team_2_score;
+        let t2 = scores.entry(team_2_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        t2.goals_scored += team_2_score;
+        t2.goals_conceded += team_1_score;
+
     }
+    println!("scores : {:#?}", scores);
+
     scores
 }
 
